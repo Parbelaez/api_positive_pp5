ade50e7aba49fcd2f0e738099ce3053989ceef97
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-01-04 21:05:32 +0000

    ROLL Back Flag

--- api_positive/settings.py
+++ api_positive/settings.py
@@ -153,7 +153,7 @@ REST_AUTH = {
     'JWT_AUTH_REFRESH_COOKIE': 'positive-refresh-token',
     # When this flag is set to false, the refresh token will be sent in the body
     # Unless, it will be only in a cookie
-    # 'JWT_AUTH_HTTPONLY': True,
+    'JWT_AUTH_HTTPONLY': False,
     'JWT_AUTH_SAMESITE': 'None',
 }
 
