8c5a12a475c49d2f58fa4e293d1b8adaf458d2dd
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-01-08 11:23:12 +0000

    Add more headers to the logger

--- middleware/dj_rest_auth_logging.py
+++ middleware/dj_rest_auth_logging.py
@@ -10,10 +10,18 @@ class LogResponseMiddleware:
         response = self.get_response(request)
         body = response.content
         headers = dict(response.items())
+        connection: respnse.get(connection, alternate=None)
+        content_length = response.get(content-length, alternate=None)
         if request.path == '/dj-rest-auth/login/':
-            logger.info(response.headers)
-            logger.info(f"Response Body: {body}")
             logger.info("Headers:")
             for header_name, header_value in headers.items():
                 logger.info(f"  {header_name}: {header_value}")
+            logger.info(f"Connection:  {connection}")
+            logger.info(f"Response Body: {body}")
+        set_cookie_headers = response.get_all('Set-Cookie')
+        if set_cookie_headers:
+            logger.info("Set-Cookie headers:")
+            for set_cookie_header in set_cookie_headers:
+                logger.info(f"  {set_cookie_header}")
+
         return response
