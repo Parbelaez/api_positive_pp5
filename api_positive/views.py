4d5b77c5681be15dc2cc4a8751f0984559aaf11a
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-01-08 11:23:37 +0000

    Change the release number

--- api_positive/views.py
+++ api_positive/views.py
@@ -8,5 +8,5 @@ def root_route(request):
         'message': 'Welcome to the Positive API',
         'version': '1.0.1',
         'author': 'Paulo Arbeláez',
-        'Release': '20240108.09.15',
+        'Release': '20240108.12.23',
     })
