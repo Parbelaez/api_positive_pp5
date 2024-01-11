69dbd0014b8494eb1cfd1b4f63773d2b6ba7b043
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-01-11 19:46:26 +0000

    Change the urls path and add force token to url user

--- api_positive/urls.py
+++ api_positive/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     ),
 
     # JWT Token Authentication
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/dj-rest-auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Apps
     path('api/profiles/', include('profiles.urls')),
