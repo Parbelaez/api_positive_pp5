bc41d9a28028f659e512c71a7e680e2f8a7c9d59
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-05-06 02:35:17 +0000

    Revert "Clone the frontend and install dependencies"
    This reverts commit 0e4d8c01d32500b4e6046329c586ad0d19fe9d24.

--- frontend/src/api/axiosDefaults.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "https://api-positive-a53d71b6a573.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
-
-export const axiosRequest = axios.create();
-export const axiosResponse = axios.create();
