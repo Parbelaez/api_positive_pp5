aadbaac876e9daa761478103e241f398df8ca1ec
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-05-06 02:03:57 +0000

    Adapt all for deployment

--- api_positive/urls.py
+++ api_positive/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from django.views.generic import TemplateView
+from .views import logout_route
 # JWT Token Authentication
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -9,24 +10,26 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('', root_route),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     # Django REST Framework
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/api-auth/', include('rest_framework.urls')),
     # dj-rest-auth for login and logout
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
     # dj-rest-auth for registration
     path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+        'api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
 
     # JWT Token Authentication
-    path('dj-rest-auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/dj-rest-auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Apps
-    path('profiles/', include('profiles.urls')),
-    path('places/', include('places.urls')),
-    path('posts/', include('posts.urls')),
-    path('likes/', include('likes.urls')),
+    path('api/', include('profiles.urls')),
+    path('api/', include('places.urls')),
+    path('api/', include('posts.urls')),
+    path('api/', include('likes.urls')),
 ]
+
+handler404 = TemplateView.as_view(template_name='index.html')
